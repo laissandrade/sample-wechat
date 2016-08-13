708d27bf04b70c7629363146834fe9748a43c74b
Ciro S. Costa <ciro9758@gmail.com>
2016-08-13 17:13:03 -0300

    update

--- bot/src/main/java/io/wedeploy/wechat/bot/ChatListenerRuntime.java
+++ bot/src/main/java/io/wedeploy/wechat/bot/ChatListenerRuntime.java
@@ -2,11 +2,17 @@ package io.wedeploy.wechat.bot;
 
 import com.wedeploy.api.WeDeploy;
 import com.wedeploy.api.realtime.RealTime;
+import com.wedeploy.api.sdk.ContentType;
 import com.wedeploy.api.sdk.Context;
 import com.wedeploy.api.sdk.Lifecycle;
+import com.wedeploy.api.serializer.Parser;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * @author cirocosta
@@ -35,10 +41,28 @@ public class ChatListenerRuntime implements Lifecycle {
 		commands = Arrays.asList(COMMANDS);
 
 		realtime = messagesClient
+			.limit(1)
+			.sort("id", "desc")
 			.watch()
 			.on("changes", res -> {
-				System.out.println("Received some messages: ");
-				System.out.println(res);
+				List<Map> messages = Parser
+					.get()
+					.parse(res[0].toString());
+
+				messages.forEach(message -> {
+					try {
+						messagesClient
+							.contentType(ContentType.JSON)
+							.post(
+								createBotMessage("Hello Fellas")
+								.toString());
+
+						System.out.println(createBotMessage("Hello Fellas"));
+
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				});
 			});
 	}
 
@@ -49,6 +73,20 @@ public class ChatListenerRuntime implements Lifecycle {
 		if (realtime != null) {
 			realtime.close();
 		}
+	}
+
+	private JSONObject createBotMessage(String content) throws JSONException {
+		JSONObject author = new JSONObject()
+			.put("id", "TheBot")
+			.put("name", "TheBot")
+			.put("color", "color-1");
+
+		JSONObject object = new JSONObject()
+			.put("content", content)
+			.put("time", "whatever")
+			.put("author", author);
+
+		return object;
 	}
 
 	private static final String BOT_PREFIX = "bot: ";
